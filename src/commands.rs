9f0576d3562441eebe754d2819cc8a4e64b471c4
Vaibhav Bisht <vaibhavbisht592@gmail.com>
2026-07-07 13:00:38 +0530

    added: command to print table names

--- src/commands.rs
+++ src/commands.rs
@@ -1,6 +1,6 @@
 use std::{fmt, str::FromStr};
 
-use crate::file::DBFile;
+use crate::{file::DBFile, page::PageType, recordformat::RecordDataType};
 
 #[derive(Debug)]
 pub enum Commands {
@@ -50,7 +50,34 @@ impl Commands {
                         println!("Overflow page")
                     }
                 },
-                Commands::Tables => {}
+                Commands::Tables => {
+                    let PageType::Btree(schemapg) = &dbfile.pages[0] else {
+                        return Err("Sqlite schema page not found!!!".into());
+                    };
+
+                    for cell in &schemapg.cells {
+                        if let Some(payload) = &cell.payload {
+                            let rowsize = payload.rows.len();
+                            let rows = &payload.rows;
+
+                            let mut rowidx = 0;
+                            // We are ignoring the the term table, the schema name last content cause that is just a CREATE query
+                            // usually.
+                            while rowidx < rowsize - 1 {
+                                if let RecordDataType::STR(data) = &rows[rowidx].content {
+                                    if data.to_lowercase() == "table" {
+                                        rowidx += 2;
+                                    } else if data.to_lowercase().starts_with("sqlite_") {
+                                        break;
+                                    } else {
+                                        println!("{data}");
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
 
